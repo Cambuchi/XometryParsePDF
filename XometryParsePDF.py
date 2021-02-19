ef37c3c5ed9a096f9306877c0e93a7929247093e
Cambuchi <49548018+Cambuchi@users.noreply.github.com>
2021-02-19 15:29:51 -0800

    Add purchase_order_process which parses Job number for renaming drawing files.

--- XometryParsePDF.py
+++ XometryParsePDF.py
@@ -10,8 +10,8 @@ import shutil
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s.%(msecs)03d: %(message)s', datefmt='%H:%M:%S')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s.%(msecs)03d: %(message)s', datefmt='%H:%M:%S')
-# logging.disable(logging.DEBUG) # un/comment to un/block debug log messages
-# logging.disable(logging.INFO) # un/comment to un/block info log messages
+logging.disable(logging.DEBUG)  # un/comment to un/block debug log messages
+# logging.disable(logging.INFO)  # un/comment to un/block info log messages
 
 
 def read_document(abs_folder_path):
@@ -19,6 +19,12 @@ def read_document(abs_folder_path):
         Depending on document content, calls the appropriate function to process. """
     # Change working directory to provided path argument.
     os.chdir(abs_folder_path)
+
+    # Pattern check for purchase orders.
+    po_regex = re.compile(r'.*(PURCHASE ORDER).*7951.*', re.DOTALL)
+
+    # Pattern check for travelers.
+    traveler_regex = re.compile(r'.*(Purchase Order)Due.*', re.DOTALL)
 
     # Searches all files in the provided directory. Does not include folders.
     for file in os.listdir('.'):
@@ -28,7 +34,7 @@ def read_document(abs_folder_path):
         if file.endswith('.pdf'):
 
             # Opens file with PyPDF2 and extracts information from the first page to determine document type.
-            logging.debug(f'{file} is a PDF, opening contents to redirect.')
+            logging.info(f'{file} is a PDF, opening contents to check document type.')
             pdf_file_obj = open(file, 'rb')
             pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
             page_obj = pdf_reader.getPage(0)
@@ -36,14 +42,18 @@ def read_document(abs_folder_path):
 
             # Closes file after extracting information needed.
             pdf_file_obj.close()
+            logging.debug(f'read_document page 1 parse: \n{sort_page}')
+
+            po_match = po_regex.match(sort_page)
+            traveler_match = traveler_regex.match(sort_page)
 
             # If initial characters match 'PURCHASE', file is a PO.
-            if sort_page[0:8] == 'PURCHASE':
+            if po_match is not None:
                 logging.info(f'{file} is an Xometry Purchase Order')
                 purchase_order_process(file)
 
             # If initial characters match 'Purchase', file is a Customer Traveler.
-            if sort_page[1:9] == 'Purchase':
+            if traveler_match is not None:
                 logging.info(f'{file} is an Xometry Traveler')
                 traveler_process(file)
 
@@ -57,7 +67,7 @@ def traveler_process(filename):
     pattern = re.compile(r'''
         .*DateContact
         ([a-zA-Z]\w{7})                                     # PO Number
-        (\d\d\/\d\d\/\d\d\d\d)                              # Due Date
+        (\d\d/\d\d/\d\d\d\d)                              # Due Date
         (.*@.*.com)                                         # Contact
         .*
         (0\w{6})                                            # Part ID
@@ -66,17 +76,7 @@ def traveler_process(filename):
         ''', re.VERBOSE | re.DOTALL)
 
     # Open traveler with PyPDF2, this time reading everything since we know it's the document we are looking for.
-    pdf_file_obj = open(filename, 'rb')
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
-    numPages = pdf_reader.numPages
-    parse_string = ''
-    for i in range(0, numPages):
-        page_obj = pdf_reader.getPage(i)
-        parse_string += page_obj.extractText()
-
-    # Close the document after getting all the information.
-    pdf_file_obj.close()
-    logging.debug(f'parse_string is: {parse_string}')
+    parse_string = open_parse_pdf(filename)
 
     matches = pattern.match(parse_string)
     logging.debug(f'PO Number is: {matches.group(1)}')
@@ -87,31 +87,44 @@ def traveler_process(filename):
     logging.debug(f'Part File is: {matches.group(5) + matches.group(6)}')
 
     # Rename drawings after we have the provided Job Number.
-    logging.debug(f'Sending {job_number} to "rename_drawings"')
+    logging.info(f'Sending {job_number} to "rename_drawings"')
     rename_drawings(job_number)
 
     # Rename travelers after we have the provided Job Number and Traveler filename.
-    logging.debug(f'Sending {filename} and {job_number} to "rename_traveler"')
+    logging.info(f'Sending {filename} and {job_number} to "rename_traveler"')
     rename_traveler(filename, job_number)
 
 
 def purchase_order_process(filename):
+    """ Opens purchase order with PyPDF and sorts information into variables.
+        Passes appropriate variables into rename_drawings and rename_traveler. """
+    logging.info(f'Processing the following Xometry PO: {filename}')
 
+    # Regex pattern to grab Part ID (also used as Job Number) from purchase orders.
+    part_id_pattern = re.compile(r'(Qty\.)(\n)(.*?)(\w{7})(\n)', re.DOTALL)
+
+    # Open traveler with PyPDF2, this time reading everything since we know it's the document we are looking for.
+    parse_string = open_parse_pdf(filename)
+
+    # Create match group for job_number, then call rename_drawings
+    job_number_match = part_id_pattern.search(parse_string)
+    job_number = job_number_match.group(4)
+    rename_drawings(job_number)
 
 
 def rename_drawings(job_number):
     """ Renames files to remove long string using the given Job Number from the Customer Traveler """
-    logging.debug(f'Renaming drawings for the following Job Number: {job_number}')
+    logging.info(f'Renaming drawings for the following Job Number: {job_number}')
 
     # Pattern that looks for files with the provided Job_Number, drawing, and drawing filetype.
-    drawing_pattern = re.compile(rf'({job_number}_r_drawing_d_).*(r_0).*(.pdf|.jpg|.jpeg|.PDF|.JPG|.JPEG)')
+    drawing_pattern = re.compile(rf'({job_number}_r_drawing_d_).*(r_0).*(\.pdf|\.jpg|\.jpeg|\.PDF|\.JPG|\.JPEG)')
 
     # Searches through all files in the current working directory (directory provided).
     for orig_filename in os.listdir('.'):
 
         # If pattern matches filename, process the filename into regex match groups.
         matches = drawing_pattern.search(orig_filename)
-        if matches == None:
+        if matches is None:
             continue
         prefix = matches.group(1)
         suffix = matches.group(2)
@@ -121,15 +134,15 @@ def rename_drawings(job_number):
         counter = 1
         new_file_name = prefix + suffix + ' (' + str(counter) + ')' + extension
 
+        # If the new_file_name is an exact match for orig_filename, it means file has already been renamed, skips.
+        if new_file_name == orig_filename:
+            logging.info(f'{new_file_name} already exists. Skipping.')
+            continue
+
         # If file name already exists, appends an incrementing number just before file extension.
         while os.path.isfile(new_file_name):
             counter += 1
             new_file_name = prefix + suffix + ' (' + str(counter) + ')' + extension
-
-        # If the new_file_name is an exact match for orig_filename, it means file has already been renamed, skips.
-        if new_file_name == orig_filename:
-            logging.debug(f'{new_file_name} already exists. Skipping.')
-            continue
 
         # After creating the filenames, move the files.
         logging.info(f'Renaming "{orig_filename}" TO "{new_file_name}"')
@@ -147,11 +160,11 @@ def rename_traveler(original_traveler, job_number):
         matches = traveler_pattern.search(orig_filename)
 
         # If pattern for file does not match, skips.
-        if matches == None:
+        if matches is None:
             continue
 
         # If match group 1 (for 'CT ') is not empty, file has already been processed, leave function.
-        if matches.group(1) != None:
+        if matches.group(1) is not None:
             logging.info(f'{orig_filename} has already been renamed. Skipping.')
             break
 
@@ -169,9 +182,24 @@ def rename_traveler(original_traveler, job_number):
             shutil.move(original_traveler, new_file_name)
 
 
+def open_parse_pdf(filename):
+    pdf_file_obj = open(filename, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+    num_pages = pdf_reader.numPages
+    parse_string = ''
+    for i in range(0, num_pages):
+        page_obj = pdf_reader.getPage(i)
+        parse_string += page_obj.extractText()
+
+    # Close the document after getting all the information.
+    pdf_file_obj.close()
+    logging.debug(f'parse_string is: \n{parse_string}')
+    return parse_string
+
+
 def main():
     folder_path = input('Please paste the absolute folder path with the files you wish to process: \n')
-    logging.debug(f'Getting data from the following directory: {folder_path}')
+    logging.info(f'Getting data from the following directory: {folder_path}')
     read_document(folder_path)
 
 
